4a4e80571250795a240a9288489c475d75f36914
Jin Hang Ng <jinhang@chalk.ai>
2023-03-09 00:41:21 -0800

    display source when failing to refresh token

--- pkg/client/tokens.go
+++ pkg/client/tokens.go
@@ -3,12 +3,13 @@ package client
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"github.com/chalk-ai/chalk-go/pkg/auth"
 	"net/http"
 	"time"
 )
 
-func (c *Client) getJwt() (*auth.JWT, error) {
+func (c *Client) getJwt() (*auth.JWT, *ChalkClientError) {
 	if c.jwt != nil && !time.Time.IsZero(c.jwt.ValidUntil) &&
 		c.jwt.ValidUntil.After(time.Now().UTC().Add(-10*time.Second)) {
 		return c.jwt, nil
@@ -20,16 +21,33 @@ func (c *Client) getJwt() (*auth.JWT, error) {
 		GrantType:    "client_credentials",
 	})
 	if err != nil {
-		return nil, err
+		return nil, &ChalkClientError{Message: err.Error()}
 	}
 
 	req, err := http.NewRequest("POST", "v1/oauth/token", bytes.NewBuffer(jsonBody))
 	if err != nil {
-		return nil, err
+		return nil, &ChalkClientError{Message: err.Error()}
 	}
 
 	response := getTokenResponse{}
 	err = c.sendRequest(req, &response)
+	if err != nil {
+		return nil, &ChalkClientError{Message: fmt.Sprintf(
+			"Error obtaining access token with these credentials: "+
+				"api_server=%q (%q), "+
+				"client_id=%q (%q), "+
+				"client_secret=%q (%q), "+
+				"environment_id=%q (%q)",
+			c.ApiServer.Value,
+			c.ApiServer.source,
+			c.ClientId.Value,
+			c.ClientId.source,
+			c.ClientSecret.Value,
+			c.ClientSecret.source,
+			c.EnvironmentId.Value,
+			c.EnvironmentId.source,
+		)}
+	}
 
 	expiry := time.Now().UTC().Add(time.Duration(response.ExpiresIn) * time.Second)
 	jwt := &auth.JWT{
