dfa8702acd21dbdedae682e9b2c4bfb732d38b09
Jin Hang Ng <jinhang@chalk.ai>
2023-03-14 13:32:57 -0700

    make unwrapFeatureInterface able to handle a pointer to any type

--- param_utils.go
+++ param_utils.go
@@ -2,7 +2,7 @@ package chalk
 
 import (
 	"fmt"
-	"time"
+	"reflect"
 	"unsafe"
 )
 
@@ -11,38 +11,47 @@ func unwrapFeature[T any](t *T) *Feature {
 }
 
 func unwrapFeatureInterface(t any) *Feature {
-	switch typed := t.(type) {
-	case *int:
-		return unwrapFeature(typed)
-	case *int8:
-		return unwrapFeature(typed)
-	case *int16:
-		return unwrapFeature(typed)
-	case *int32:
-		return unwrapFeature(typed)
-	case *int64:
-		return unwrapFeature(typed)
-	case *uint:
-		return unwrapFeature(typed)
-	case *uint8:
-		return unwrapFeature(typed)
-	case *uint16:
-		return unwrapFeature(typed)
-	case *uint32:
-		return unwrapFeature(typed)
-	case *uint64:
-		return unwrapFeature(typed)
-	case *string:
-		return unwrapFeature(typed)
-	case *float32:
-		return unwrapFeature(typed)
-	case *float64:
-		return unwrapFeature(typed)
-	case *bool:
-		return unwrapFeature(typed)
-	case *time.Time:
-		return unwrapFeature(typed)
-	default:
-		panic(fmt.Sprintf("unsupported type: %s", typed))
+	if reflect.ValueOf(t).Kind() == reflect.Ptr {
+		v := reflect.New(reflect.TypeOf(t))
+		v.Elem().Set(reflect.ValueOf(t))
+		return (*Feature)(v.Elem().UnsafePointer())
 	}
+
+	typePointedTo := reflect.ValueOf(t).Elem().Type()
+	panic(fmt.Sprintf("unsupported type: %s", typePointedTo))
+
+	//switch typed := t.(type) {
+	//case *any:
+	//	return unwrapFeature(typed)
+	//case *int8:
+	//	return unwrapFeature(typed)
+	//case *int16:
+	//	return unwrapFeature(typed)
+	//case *int32:
+	//	return unwrapFeature(typed)
+	//case *int64:
+	//	return unwrapFeature(typed)
+	//case *uint:
+	//	return unwrapFeature(typed)
+	//case *uint8:
+	//	return unwrapFeature(typed)
+	//case *uint16:
+	//	return unwrapFeature(typed)
+	//case *uint32:
+	//	return unwrapFeature(typed)
+	//case *uint64:
+	//	return unwrapFeature(typed)
+	//case *string:
+	//	return unwrapFeature(typed)
+	//case *float32:
+	//	return unwrapFeature(typed)
+	//case *float64:
+	//	return unwrapFeature(typed)
+	//case *bool:
+	//	return unwrapFeature(typed)
+	//case *time.Time:
+	//	return unwrapFeature(typed)
+	//default:
+	// 	panic(fmt.Sprintf("unsupported type: %s", reflect.TypeOf(t)))
+	//}
 }
