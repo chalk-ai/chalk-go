fcc1d49743b7ff267b176d3ca6a9d80d140d5ef7
rooftoofwoof <jinhang@chalk.ai>
2024-05-07 19:15:43 -0700

    add unmarshal windowed features test

--- unmarshal_test.go
+++ unmarshal_test.go
@@ -7,10 +7,15 @@ import (
 )
 
 type unmarshalUser struct {
-	Id      *string
+	Id *string
+
+	// Versioned features
 	Grade   *int `versioned:"default(2)"`
 	GradeV1 *int `versioned:"true"`
 	GradeV2 *int `versioned:"true"`
+
+	// Windowed features
+	AvgSpend map[string]*float64 `windows:"1m,5m,1h"`
 }
 
 func TestUnmarshalVersionedFeatures(t *testing.T) {
@@ -47,4 +52,48 @@ func TestUnmarshalVersionedFeatures(t *testing.T) {
 	assert.Equal(t, 2, *user.Grade)
 	assert.Equal(t, 1, *user.GradeV1)
 	assert.Equal(t, 2, *user.GradeV2)
+}
+
+func TestUnmarshalWindowedFeatures(t *testing.T) {
+	data := []FeatureResult{
+		{
+			Field:     "unmarshal_user.avg_spend__60__",
+			Value:     60.0,
+			Pkey:      "khjdsfjhdksjfh",
+			Timestamp: time.Time{},
+			Meta:      nil,
+			Error:     nil,
+		},
+		{
+			Field:     "unmarshal_user.avg_spend__300__",
+			Value:     300.0,
+			Pkey:      "kjhsdfkjkdjfk",
+			Timestamp: time.Time{},
+			Meta:      nil,
+			Error:     nil,
+		},
+		{
+			Field:     "unmarshal_user.avg_spend__3600__",
+			Value:     3600.0,
+			Pkey:      "kjhsdfkjkdjfk",
+			Timestamp: time.Time{},
+			Meta:      nil,
+			Error:     nil,
+		},
+	}
+	result := OnlineQueryResult{
+		Data:            data,
+		Meta:            nil,
+		features:        nil,
+		expectedOutputs: nil,
+	}
+	user := unmarshalUser{}
+	unmarshalErr := result.UnmarshalInto(&user)
+	if unmarshalErr != nil {
+		t.Fatal(unmarshalErr)
+	}
+	assert.Nil(t, unmarshalErr)
+	assert.Equal(t, 60.0, *user.AvgSpend["1m"])
+	assert.Equal(t, 300.0, *user.AvgSpend["5m"])
+	assert.Equal(t, 3600.0, *user.AvgSpend["1h"])
 }
