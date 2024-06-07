d41b50576aaf9199a4d022c257a5776a1feb5ff3
rooftoofwoof <jinhang@chalk.ai>
2024-06-07 11:22:45 -0700

    add feather test

---
+++ internal/feather_test.go
@@ -0,0 +1,33 @@
+package internal
+
+import (
+	assert "github.com/stretchr/testify/require"
+	"reflect"
+	"testing"
+)
+
+func TestColumnMapToRecordOptionalPrimitives(t *testing.T) {
+	// Tests that we can convert a slice of pointers to primitive
+	// types to an Arrow column with nulls correctly set.
+	num := int64(1)
+	numFloat := 1.0
+	in := map[string]any{
+		"int":   []*int64{&num, nil, &num},
+		"float": []*float64{&numFloat, &numFloat, nil},
+	}
+	record, convertErr := ColumnMapToRecord(in)
+	assert.Nil(t, convertErr)
+	for i := 0; i < int(record.NumRows()); i++ {
+		for j, col := range record.Columns() {
+			name := record.ColumnName(j)
+			slice := in[name]
+			if col.IsNull(i) {
+				assert.True(t, reflect.ValueOf(slice).Index(i).IsNil())
+			} else {
+				val, valErr := GetValueFromArrowArray(col, i)
+				assert.Nil(t, valErr)
+				assert.Equal(t, reflect.ValueOf(slice).Index(i).Elem().Interface(), val)
+			}
+		}
+	}
+}
