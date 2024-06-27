28e61640645047762294e5ee7957aaaf6984ffb9
rooftoofwoof <jinhang@chalk.ai>
2024-06-27 15:07:38 -0700

    nested

--- test_fixtures.go
+++ test_fixtures.go
@@ -18,26 +18,43 @@ type possessions struct {
 	Plane *string
 }
 
+type grandparent struct {
+	Name *string `dataclass_field:"true"`
+}
+
+type parent struct {
+	Name *string `dataclass_field:"true"`
+	Mom  *grandparent
+	Dad  *grandparent
+}
+
+type child struct {
+	Name *string `dataclass_field:"true"`
+	Mom  *parent
+	Dad  *parent
+}
+
 type anotherFeature struct {
 	Id *string
 }
 
 type allTypes struct {
-	Int                  *int64
-	Float                *float64
-	String               *string
-	Bool                 *bool
-	Timestamp            *time.Time
-	IntList              *[]int64
-	NestedIntPointerList *[]*[]int64
-	NestedIntList        *[][]int64
-	WindowedInt          map[string]*int64   `windows:"1m,5m,1h"`
-	WindowedList         map[string]*[]int64 `windows:"1m"`
-	Dataclass            *testLatLng         `dataclass:"true"`
-	DataclassList        *[]testLatLng
-	DataclassWithList    *favoriteThings
-	DataclassWithNils    *possessions
-	Nested               *anotherFeature
+	Int                    *int64
+	Float                  *float64
+	String                 *string
+	Bool                   *bool
+	Timestamp              *time.Time
+	IntList                *[]int64
+	NestedIntPointerList   *[]*[]int64
+	NestedIntList          *[][]int64
+	WindowedInt            map[string]*int64   `windows:"1m,5m,1h"`
+	WindowedList           map[string]*[]int64 `windows:"1m"`
+	Dataclass              *testLatLng         `dataclass:"true"`
+	DataclassList          *[]testLatLng
+	DataclassWithList      *favoriteThings
+	DataclassWithNils      *possessions
+	DataclassWithDataclass *child
+	Nested                 *anotherFeature
 }
 
 var testRootFeatures struct {
