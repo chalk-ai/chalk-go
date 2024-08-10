2254d5e2eb39313f80abe7206adcde90cf598449
rooftoofwoof <jinhang@chalk.ai>
2024-08-09 22:08:22 -0700

    test native grpc update aggs test

--- internal/tests/integration/grpc_upload_test.go
+++ internal/tests/integration/grpc_upload_test.go
@@ -1,6 +1,7 @@
 package integration
 
 import (
+	"context"
 	"github.com/chalk-ai/chalk-go"
 	assert "github.com/stretchr/testify/require"
 	"math/rand"
@@ -39,53 +40,7 @@ func getRandomInts(n int) []int64 {
 	return result
 }
 
-// TestGrpcUpdateAggregates is a separate test from the non-grpc upload test
-// because the GRPC endpoint only handles uploading windowed agg aggregations.
-func TestGrpcUpdateAggregates(t *testing.T) {
-	t.Parallel()
-	SkipIfNotIntegrationTester(t)
-	assert.NoError(t, chalk.InitFeatures(&Features))
-	client, err := chalk.NewClient(&chalk.ClientConfig{UseGrpc: true})
-	if err != nil {
-		t.Fatal("Failed creating a Chalk Client", err)
-	}
-
-	distinctProofIds := getRandomInts(4)
-	proofIds := []int64{
-		distinctProofIds[0],
-		distinctProofIds[0],
-		distinctProofIds[1],
-		distinctProofIds[2],
-		distinctProofIds[3],
-	}
-	theoremIds := getRandomInts(len(proofIds))
-	now := time.Now().UTC()
-	params := chalk.UpdateAggregatesParams{Inputs: map[any]any{
-		Features.Theorem.Id: theoremIds,
-		// This should be an int64 once the server automatically casts group by columns to binary
-		Features.Theorem.ProofId: proofIds,
-		// This should be its original int64 type once the server automatically casts aggregated columns to float64
-		Features.Theorem.NumLines: []int64{34, 55, 89, 144, 233},
-		// This should be a string once the server automatically casts group by columns to binary
-		Features.Theorem.Author: []string{
-			"Carl Friederich Gauss",
-			"Sir Isaac Newton",
-			"Albert Einstein",
-			"Richard Feynman",
-			"Sir Roger Penrose",
-		},
-		"__ts__": []time.Time{
-			now.Add(-1 * 24 * time.Hour),
-			now.Add(-2 * 24 * time.Hour),
-			now.Add(-3 * 24 * time.Hour),
-			now.Add(-4 * 24 * time.Hour),
-			now.Add(-5 * 24 * time.Hour),
-		},
-	}}
-
-	_, err = client.UpdateAggregates(params)
-	assert.NoError(t, err)
-
+func verifyUpdateAggregateResults(t *testing.T, distinctProofIds []int64, client chalk.Client) {
 	// Query aggregation results
 	queryParams := chalk.OnlineQueryParams{}.WithInput(
 		Features.Proof.Id,
@@ -124,4 +79,87 @@ func TestGrpcUpdateAggregates(t *testing.T) {
 	assert.Equal(t, int64(233), *proofResults[3].TotalTheoremLines["90d"])
 	assert.Equal(t, float64(233), *proofResults[3].MeanTheoremLines["30d"])
 	assert.Equal(t, float64(233), *proofResults[3].MeanTheoremLines["90d"])
+}
+
+func getUpdateAggregateParams(proofIds []int64, theoremIds []int64, now time.Time) chalk.UpdateAggregatesParams {
+	return chalk.UpdateAggregatesParams{Inputs: map[any]any{
+		Features.Theorem.Id:       theoremIds,
+		Features.Theorem.ProofId:  proofIds,
+		Features.Theorem.NumLines: []int64{34, 55, 89, 144, 233},
+		Features.Theorem.Author: []string{
+			"Carl Friederich Gauss",
+			"Sir Isaac Newton",
+			"Albert Einstein",
+			"Richard Feynman",
+			"Sir Roger Penrose",
+		},
+		"__ts__": []time.Time{
+			now.Add(-1 * 24 * time.Hour),
+			now.Add(-2 * 24 * time.Hour),
+			now.Add(-3 * 24 * time.Hour),
+			now.Add(-4 * 24 * time.Hour),
+			now.Add(-5 * 24 * time.Hour),
+		},
+	}}
+}
+
+// TestGrpcUpdateAggregates is a separate test from the non-grpc upload test
+// because the GRPC endpoint only handles uploading windowed agg aggregations.
+func TestGrpcUpdateAggregates(t *testing.T) {
+	t.Parallel()
+	SkipIfNotIntegrationTester(t)
+	assert.NoError(t, chalk.InitFeatures(&Features))
+	client, err := chalk.NewClient(&chalk.ClientConfig{UseGrpc: true})
+	if err != nil {
+		t.Fatal("Failed creating a Chalk Client", err)
+	}
+
+	distinctProofIds := getRandomInts(4)
+	proofIds := []int64{
+		distinctProofIds[0],
+		distinctProofIds[0],
+		distinctProofIds[1],
+		distinctProofIds[2],
+		distinctProofIds[3],
+	}
+	theoremIds := getRandomInts(len(proofIds))
+	now := time.Now().UTC()
+	params := getUpdateAggregateParams(proofIds, theoremIds, now)
+	_, err = client.UpdateAggregates(params)
+	assert.NoError(t, err)
+	// Query aggregation results
+	verifyUpdateAggregateResults(t, distinctProofIds, client)
+}
+
+// TestGrpcUpdateAggregatesNative tests the same functionality as TestGrpcUpdateAggregates
+// just from the native gRPC client.
+func TestGrpcUpdateAggregatesNative(t *testing.T) {
+	t.Parallel()
+	SkipIfNotIntegrationTester(t)
+	assert.NoError(t, chalk.InitFeatures(&Features))
+	client, err := chalk.NewGRPCClient()
+	if err != nil {
+		t.Fatal("Failed creating a Chalk Client", err)
+	}
+
+	distinctProofIds := getRandomInts(4)
+	proofIds := []int64{
+		distinctProofIds[0],
+		distinctProofIds[0],
+		distinctProofIds[1],
+		distinctProofIds[2],
+		distinctProofIds[3],
+	}
+	theoremIds := getRandomInts(len(proofIds))
+	now := time.Now().UTC()
+	params := getUpdateAggregateParams(proofIds, theoremIds, now)
+	_, err = client.UpdateAggregates(context.Background(), params)
+	assert.NoError(t, err)
+
+	restClient, err := chalk.NewClient()
+	if err != nil {
+		t.Fatal("Failed creating a REST client", err)
+	}
+	// Query aggregation results
+	verifyUpdateAggregateResults(t, distinctProofIds, restClient)
 }
