ed8920273e44952eddc3511ac657163e8bbf04e3
Sai Atmakuri <sai@chalk.ai>
2024-10-27 19:25:03 -0700

    Update protos

--- gen/chalk/server/v1/serverv1connect/builder.connect.go
+++ gen/chalk/server/v1/serverv1connect/builder.connect.go
@@ -51,17 +51,41 @@ const (
 	// BuilderServiceUploadSourceProcedure is the fully-qualified name of the BuilderService's
 	// UploadSource RPC.
 	BuilderServiceUploadSourceProcedure = "/chalk.server.v1.BuilderService/UploadSource"
+	// BuilderServiceGetDeploymentStepsProcedure is the fully-qualified name of the BuilderService's
+	// GetDeploymentSteps RPC.
+	BuilderServiceGetDeploymentStepsProcedure = "/chalk.server.v1.BuilderService/GetDeploymentSteps"
+	// BuilderServiceGetDeploymentLogsProcedure is the fully-qualified name of the BuilderService's
+	// GetDeploymentLogs RPC.
+	BuilderServiceGetDeploymentLogsProcedure = "/chalk.server.v1.BuilderService/GetDeploymentLogs"
+	// BuilderServiceGetClusterTimescaleDBProcedure is the fully-qualified name of the BuilderService's
+	// GetClusterTimescaleDB RPC.
+	BuilderServiceGetClusterTimescaleDBProcedure = "/chalk.server.v1.BuilderService/GetClusterTimescaleDB"
+	// BuilderServiceGetClusterGatewayProcedure is the fully-qualified name of the BuilderService's
+	// GetClusterGateway RPC.
+	BuilderServiceGetClusterGatewayProcedure = "/chalk.server.v1.BuilderService/GetClusterGateway"
+	// BuilderServiceCreateClusterTimescaleDBProcedure is the fully-qualified name of the
+	// BuilderService's CreateClusterTimescaleDB RPC.
+	BuilderServiceCreateClusterTimescaleDBProcedure = "/chalk.server.v1.BuilderService/CreateClusterTimescaleDB"
+	// BuilderServiceCreateClusterGatewayProcedure is the fully-qualified name of the BuilderService's
+	// CreateClusterGateway RPC.
+	BuilderServiceCreateClusterGatewayProcedure = "/chalk.server.v1.BuilderService/CreateClusterGateway"
 )
 
 // These variables are the protoreflect.Descriptor objects for the RPCs defined in this package.
 var (
-	builderServiceServiceDescriptor                    = v1.File_chalk_server_v1_builder_proto.Services().ByName("BuilderService")
-	builderServiceActivateDeploymentMethodDescriptor   = builderServiceServiceDescriptor.Methods().ByName("ActivateDeployment")
-	builderServiceIndexDeploymentMethodDescriptor      = builderServiceServiceDescriptor.Methods().ByName("IndexDeployment")
-	builderServiceDeployKubeComponentsMethodDescriptor = builderServiceServiceDescriptor.Methods().ByName("DeployKubeComponents")
-	builderServiceRebuildDeploymentMethodDescriptor    = builderServiceServiceDescriptor.Methods().ByName("RebuildDeployment")
-	builderServiceRedeployDeploymentMethodDescriptor   = builderServiceServiceDescriptor.Methods().ByName("RedeployDeployment")
-	builderServiceUploadSourceMethodDescriptor         = builderServiceServiceDescriptor.Methods().ByName("UploadSource")
+	builderServiceServiceDescriptor                        = v1.File_chalk_server_v1_builder_proto.Services().ByName("BuilderService")
+	builderServiceActivateDeploymentMethodDescriptor       = builderServiceServiceDescriptor.Methods().ByName("ActivateDeployment")
+	builderServiceIndexDeploymentMethodDescriptor          = builderServiceServiceDescriptor.Methods().ByName("IndexDeployment")
+	builderServiceDeployKubeComponentsMethodDescriptor     = builderServiceServiceDescriptor.Methods().ByName("DeployKubeComponents")
+	builderServiceRebuildDeploymentMethodDescriptor        = builderServiceServiceDescriptor.Methods().ByName("RebuildDeployment")
+	builderServiceRedeployDeploymentMethodDescriptor       = builderServiceServiceDescriptor.Methods().ByName("RedeployDeployment")
+	builderServiceUploadSourceMethodDescriptor             = builderServiceServiceDescriptor.Methods().ByName("UploadSource")
+	builderServiceGetDeploymentStepsMethodDescriptor       = builderServiceServiceDescriptor.Methods().ByName("GetDeploymentSteps")
+	builderServiceGetDeploymentLogsMethodDescriptor        = builderServiceServiceDescriptor.Methods().ByName("GetDeploymentLogs")
+	builderServiceGetClusterTimescaleDBMethodDescriptor    = builderServiceServiceDescriptor.Methods().ByName("GetClusterTimescaleDB")
+	builderServiceGetClusterGatewayMethodDescriptor        = builderServiceServiceDescriptor.Methods().ByName("GetClusterGateway")
+	builderServiceCreateClusterTimescaleDBMethodDescriptor = builderServiceServiceDescriptor.Methods().ByName("CreateClusterTimescaleDB")
+	builderServiceCreateClusterGatewayMethodDescriptor     = builderServiceServiceDescriptor.Methods().ByName("CreateClusterGateway")
 )
 
 // BuilderServiceClient is a client for the chalk.server.v1.BuilderService service.
@@ -80,6 +104,12 @@ type BuilderServiceClient interface {
 	RedeployDeployment(context.Context, *connect.Request[v1.RedeployDeploymentRequest]) (*connect.Response[v1.RedeployDeploymentResponse], error)
 	// Triggers a new build with the provided source code archive and deploys the result
 	UploadSource(context.Context, *connect.Request[v1.UploadSourceRequest]) (*connect.Response[v1.UploadSourceResponse], error)
+	GetDeploymentSteps(context.Context, *connect.Request[v1.GetDeploymentStepsRequest]) (*connect.Response[v1.GetDeploymentStepsResponse], error)
+	GetDeploymentLogs(context.Context, *connect.Request[v1.GetDeploymentLogsRequest]) (*connect.Response[v1.GetDeploymentLogsResponse], error)
+	GetClusterTimescaleDB(context.Context, *connect.Request[v1.GetClusterTimescaleDBRequest]) (*connect.Response[v1.GetClusterTimescaleDBResponse], error)
+	GetClusterGateway(context.Context, *connect.Request[v1.GetClusterGatewayRequest]) (*connect.Response[v1.GetClusterGatewayResponse], error)
+	CreateClusterTimescaleDB(context.Context, *connect.Request[v1.CreateClusterTimescaleDBRequest]) (*connect.Response[v1.CreateClusterTimescaleDBResponse], error)
+	CreateClusterGateway(context.Context, *connect.Request[v1.CreateClusterGatewayRequest]) (*connect.Response[v1.CreateClusterGatewayResponse], error)
 }
 
 // NewBuilderServiceClient constructs a client for the chalk.server.v1.BuilderService service. By
@@ -128,17 +158,59 @@ func NewBuilderServiceClient(httpClient connect.HTTPClient, baseURL string, opts
 			connect.WithSchema(builderServiceUploadSourceMethodDescriptor),
 			connect.WithClientOptions(opts...),
 		),
+		getDeploymentSteps: connect.NewClient[v1.GetDeploymentStepsRequest, v1.GetDeploymentStepsResponse](
+			httpClient,
+			baseURL+BuilderServiceGetDeploymentStepsProcedure,
+			connect.WithSchema(builderServiceGetDeploymentStepsMethodDescriptor),
+			connect.WithClientOptions(opts...),
+		),
+		getDeploymentLogs: connect.NewClient[v1.GetDeploymentLogsRequest, v1.GetDeploymentLogsResponse](
+			httpClient,
+			baseURL+BuilderServiceGetDeploymentLogsProcedure,
+			connect.WithSchema(builderServiceGetDeploymentLogsMethodDescriptor),
+			connect.WithClientOptions(opts...),
+		),
+		getClusterTimescaleDB: connect.NewClient[v1.GetClusterTimescaleDBRequest, v1.GetClusterTimescaleDBResponse](
+			httpClient,
+			baseURL+BuilderServiceGetClusterTimescaleDBProcedure,
+			connect.WithSchema(builderServiceGetClusterTimescaleDBMethodDescriptor),
+			connect.WithClientOptions(opts...),
+		),
+		getClusterGateway: connect.NewClient[v1.GetClusterGatewayRequest, v1.GetClusterGatewayResponse](
+			httpClient,
+			baseURL+BuilderServiceGetClusterGatewayProcedure,
+			connect.WithSchema(builderServiceGetClusterGatewayMethodDescriptor),
+			connect.WithClientOptions(opts...),
+		),
+		createClusterTimescaleDB: connect.NewClient[v1.CreateClusterTimescaleDBRequest, v1.CreateClusterTimescaleDBResponse](
+			httpClient,
+			baseURL+BuilderServiceCreateClusterTimescaleDBProcedure,
+			connect.WithSchema(builderServiceCreateClusterTimescaleDBMethodDescriptor),
+			connect.WithClientOptions(opts...),
+		),
+		createClusterGateway: connect.NewClient[v1.CreateClusterGatewayRequest, v1.CreateClusterGatewayResponse](
+			httpClient,
+			baseURL+BuilderServiceCreateClusterGatewayProcedure,
+			connect.WithSchema(builderServiceCreateClusterGatewayMethodDescriptor),
+			connect.WithClientOptions(opts...),
+		),
 	}
 }
 
 // builderServiceClient implements BuilderServiceClient.
 type builderServiceClient struct {
-	activateDeployment   *connect.Client[v1.ActivateDeploymentRequest, v1.ActivateDeploymentResponse]
-	indexDeployment      *connect.Client[v1.IndexDeploymentRequest, v1.IndexDeploymentResponse]
-	deployKubeComponents *connect.Client[v1.DeployKubeComponentsRequest, v1.DeployKubeComponentsResponse]
-	rebuildDeployment    *connect.Client[v1.RebuildDeploymentRequest, v1.RebuildDeploymentResponse]
-	redeployDeployment   *connect.Client[v1.RedeployDeploymentRequest, v1.RedeployDeploymentResponse]
-	uploadSource         *connect.Client[v1.UploadSourceRequest, v1.UploadSourceResponse]
+	activateDeployment       *connect.Client[v1.ActivateDeploymentRequest, v1.ActivateDeploymentResponse]
+	indexDeployment          *connect.Client[v1.IndexDeploymentRequest, v1.IndexDeploymentResponse]
+	deployKubeComponents     *connect.Client[v1.DeployKubeComponentsRequest, v1.DeployKubeComponentsResponse]
+	rebuildDeployment        *connect.Client[v1.RebuildDeploymentRequest, v1.RebuildDeploymentResponse]
+	redeployDeployment       *connect.Client[v1.RedeployDeploymentRequest, v1.RedeployDeploymentResponse]
+	uploadSource             *connect.Client[v1.UploadSourceRequest, v1.UploadSourceResponse]
+	getDeploymentSteps       *connect.Client[v1.GetDeploymentStepsRequest, v1.GetDeploymentStepsResponse]
+	getDeploymentLogs        *connect.Client[v1.GetDeploymentLogsRequest, v1.GetDeploymentLogsResponse]
+	getClusterTimescaleDB    *connect.Client[v1.GetClusterTimescaleDBRequest, v1.GetClusterTimescaleDBResponse]
+	getClusterGateway        *connect.Client[v1.GetClusterGatewayRequest, v1.GetClusterGatewayResponse]
+	createClusterTimescaleDB *connect.Client[v1.CreateClusterTimescaleDBRequest, v1.CreateClusterTimescaleDBResponse]
+	createClusterGateway     *connect.Client[v1.CreateClusterGatewayRequest, v1.CreateClusterGatewayResponse]
 }
 
 // ActivateDeployment calls chalk.server.v1.BuilderService.ActivateDeployment.
@@ -171,6 +243,36 @@ func (c *builderServiceClient) UploadSource(ctx context.Context, req *connect.Re
 	return c.uploadSource.CallUnary(ctx, req)
 }
 
+// GetDeploymentSteps calls chalk.server.v1.BuilderService.GetDeploymentSteps.
+func (c *builderServiceClient) GetDeploymentSteps(ctx context.Context, req *connect.Request[v1.GetDeploymentStepsRequest]) (*connect.Response[v1.GetDeploymentStepsResponse], error) {
+	return c.getDeploymentSteps.CallUnary(ctx, req)
+}
+
+// GetDeploymentLogs calls chalk.server.v1.BuilderService.GetDeploymentLogs.
+func (c *builderServiceClient) GetDeploymentLogs(ctx context.Context, req *connect.Request[v1.GetDeploymentLogsRequest]) (*connect.Response[v1.GetDeploymentLogsResponse], error) {
+	return c.getDeploymentLogs.CallUnary(ctx, req)
+}
+
+// GetClusterTimescaleDB calls chalk.server.v1.BuilderService.GetClusterTimescaleDB.
+func (c *builderServiceClient) GetClusterTimescaleDB(ctx context.Context, req *connect.Request[v1.GetClusterTimescaleDBRequest]) (*connect.Response[v1.GetClusterTimescaleDBResponse], error) {
+	return c.getClusterTimescaleDB.CallUnary(ctx, req)
+}
+
+// GetClusterGateway calls chalk.server.v1.BuilderService.GetClusterGateway.
+func (c *builderServiceClient) GetClusterGateway(ctx context.Context, req *connect.Request[v1.GetClusterGatewayRequest]) (*connect.Response[v1.GetClusterGatewayResponse], error) {
+	return c.getClusterGateway.CallUnary(ctx, req)
+}
+
+// CreateClusterTimescaleDB calls chalk.server.v1.BuilderService.CreateClusterTimescaleDB.
+func (c *builderServiceClient) CreateClusterTimescaleDB(ctx context.Context, req *connect.Request[v1.CreateClusterTimescaleDBRequest]) (*connect.Response[v1.CreateClusterTimescaleDBResponse], error) {
+	return c.createClusterTimescaleDB.CallUnary(ctx, req)
+}
+
+// CreateClusterGateway calls chalk.server.v1.BuilderService.CreateClusterGateway.
+func (c *builderServiceClient) CreateClusterGateway(ctx context.Context, req *connect.Request[v1.CreateClusterGatewayRequest]) (*connect.Response[v1.CreateClusterGatewayResponse], error) {
+	return c.createClusterGateway.CallUnary(ctx, req)
+}
+
 // BuilderServiceHandler is an implementation of the chalk.server.v1.BuilderService service.
 type BuilderServiceHandler interface {
 	// Takes an existing (past) deployment and promotes the k8s resources / other things associated with it.
@@ -187,6 +289,12 @@ type BuilderServiceHandler interface {
 	RedeployDeployment(context.Context, *connect.Request[v1.RedeployDeploymentRequest]) (*connect.Response[v1.RedeployDeploymentResponse], error)
 	// Triggers a new build with the provided source code archive and deploys the result
 	UploadSource(context.Context, *connect.Request[v1.UploadSourceRequest]) (*connect.Response[v1.UploadSourceResponse], error)
+	GetDeploymentSteps(context.Context, *connect.Request[v1.GetDeploymentStepsRequest]) (*connect.Response[v1.GetDeploymentStepsResponse], error)
+	GetDeploymentLogs(context.Context, *connect.Request[v1.GetDeploymentLogsRequest]) (*connect.Response[v1.GetDeploymentLogsResponse], error)
+	GetClusterTimescaleDB(context.Context, *connect.Request[v1.GetClusterTimescaleDBRequest]) (*connect.Response[v1.GetClusterTimescaleDBResponse], error)
+	GetClusterGateway(context.Context, *connect.Request[v1.GetClusterGatewayRequest]) (*connect.Response[v1.GetClusterGatewayResponse], error)
+	CreateClusterTimescaleDB(context.Context, *connect.Request[v1.CreateClusterTimescaleDBRequest]) (*connect.Response[v1.CreateClusterTimescaleDBResponse], error)
+	CreateClusterGateway(context.Context, *connect.Request[v1.CreateClusterGatewayRequest]) (*connect.Response[v1.CreateClusterGatewayResponse], error)
 }
 
 // NewBuilderServiceHandler builds an HTTP handler from the service implementation. It returns the
@@ -231,6 +339,42 @@ func NewBuilderServiceHandler(svc BuilderServiceHandler, opts ...connect.Handler
 		connect.WithSchema(builderServiceUploadSourceMethodDescriptor),
 		connect.WithHandlerOptions(opts...),
 	)
+	builderServiceGetDeploymentStepsHandler := connect.NewUnaryHandler(
+		BuilderServiceGetDeploymentStepsProcedure,
+		svc.GetDeploymentSteps,
+		connect.WithSchema(builderServiceGetDeploymentStepsMethodDescriptor),
+		connect.WithHandlerOptions(opts...),
+	)
+	builderServiceGetDeploymentLogsHandler := connect.NewUnaryHandler(
+		BuilderServiceGetDeploymentLogsProcedure,
+		svc.GetDeploymentLogs,
+		connect.WithSchema(builderServiceGetDeploymentLogsMethodDescriptor),
+		connect.WithHandlerOptions(opts...),
+	)
+	builderServiceGetClusterTimescaleDBHandler := connect.NewUnaryHandler(
+		BuilderServiceGetClusterTimescaleDBProcedure,
+		svc.GetClusterTimescaleDB,
+		connect.WithSchema(builderServiceGetClusterTimescaleDBMethodDescriptor),
+		connect.WithHandlerOptions(opts...),
+	)
+	builderServiceGetClusterGatewayHandler := connect.NewUnaryHandler(
+		BuilderServiceGetClusterGatewayProcedure,
+		svc.GetClusterGateway,
+		connect.WithSchema(builderServiceGetClusterGatewayMethodDescriptor),
+		connect.WithHandlerOptions(opts...),
+	)
+	builderServiceCreateClusterTimescaleDBHandler := connect.NewUnaryHandler(
+		BuilderServiceCreateClusterTimescaleDBProcedure,
+		svc.CreateClusterTimescaleDB,
+		connect.WithSchema(builderServiceCreateClusterTimescaleDBMethodDescriptor),
+		connect.WithHandlerOptions(opts...),
+	)
+	builderServiceCreateClusterGatewayHandler := connect.NewUnaryHandler(
+		BuilderServiceCreateClusterGatewayProcedure,
+		svc.CreateClusterGateway,
+		connect.WithSchema(builderServiceCreateClusterGatewayMethodDescriptor),
+		connect.WithHandlerOptions(opts...),
+	)
 	return "/chalk.server.v1.BuilderService/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		switch r.URL.Path {
 		case BuilderServiceActivateDeploymentProcedure:
@@ -245,6 +389,18 @@ func NewBuilderServiceHandler(svc BuilderServiceHandler, opts ...connect.Handler
 			builderServiceRedeployDeploymentHandler.ServeHTTP(w, r)
 		case BuilderServiceUploadSourceProcedure:
 			builderServiceUploadSourceHandler.ServeHTTP(w, r)
+		case BuilderServiceGetDeploymentStepsProcedure:
+			builderServiceGetDeploymentStepsHandler.ServeHTTP(w, r)
+		case BuilderServiceGetDeploymentLogsProcedure:
+			builderServiceGetDeploymentLogsHandler.ServeHTTP(w, r)
+		case BuilderServiceGetClusterTimescaleDBProcedure:
+			builderServiceGetClusterTimescaleDBHandler.ServeHTTP(w, r)
+		case BuilderServiceGetClusterGatewayProcedure:
+			builderServiceGetClusterGatewayHandler.ServeHTTP(w, r)
+		case BuilderServiceCreateClusterTimescaleDBProcedure:
+			builderServiceCreateClusterTimescaleDBHandler.ServeHTTP(w, r)
+		case BuilderServiceCreateClusterGatewayProcedure:
+			builderServiceCreateClusterGatewayHandler.ServeHTTP(w, r)
 		default:
 			http.NotFound(w, r)
 		}
@@ -276,4 +432,28 @@ func (UnimplementedBuilderServiceHandler) RedeployDeployment(context.Context, *c
 
 func (UnimplementedBuilderServiceHandler) UploadSource(context.Context, *connect.Request[v1.UploadSourceRequest]) (*connect.Response[v1.UploadSourceResponse], error) {
 	return nil, connect.NewError(connect.CodeUnimplemented, errors.New("chalk.server.v1.BuilderService.UploadSource is not implemented"))
+}
+
+func (UnimplementedBuilderServiceHandler) GetDeploymentSteps(context.Context, *connect.Request[v1.GetDeploymentStepsRequest]) (*connect.Response[v1.GetDeploymentStepsResponse], error) {
+	return nil, connect.NewError(connect.CodeUnimplemented, errors.New("chalk.server.v1.BuilderService.GetDeploymentSteps is not implemented"))
+}
+
+func (UnimplementedBuilderServiceHandler) GetDeploymentLogs(context.Context, *connect.Request[v1.GetDeploymentLogsRequest]) (*connect.Response[v1.GetDeploymentLogsResponse], error) {
+	return nil, connect.NewError(connect.CodeUnimplemented, errors.New("chalk.server.v1.BuilderService.GetDeploymentLogs is not implemented"))
+}
+
+func (UnimplementedBuilderServiceHandler) GetClusterTimescaleDB(context.Context, *connect.Request[v1.GetClusterTimescaleDBRequest]) (*connect.Response[v1.GetClusterTimescaleDBResponse], error) {
+	return nil, connect.NewError(connect.CodeUnimplemented, errors.New("chalk.server.v1.BuilderService.GetClusterTimescaleDB is not implemented"))
+}
+
+func (UnimplementedBuilderServiceHandler) GetClusterGateway(context.Context, *connect.Request[v1.GetClusterGatewayRequest]) (*connect.Response[v1.GetClusterGatewayResponse], error) {
+	return nil, connect.NewError(connect.CodeUnimplemented, errors.New("chalk.server.v1.BuilderService.GetClusterGateway is not implemented"))
+}
+
+func (UnimplementedBuilderServiceHandler) CreateClusterTimescaleDB(context.Context, *connect.Request[v1.CreateClusterTimescaleDBRequest]) (*connect.Response[v1.CreateClusterTimescaleDBResponse], error) {
+	return nil, connect.NewError(connect.CodeUnimplemented, errors.New("chalk.server.v1.BuilderService.CreateClusterTimescaleDB is not implemented"))
+}
+
+func (UnimplementedBuilderServiceHandler) CreateClusterGateway(context.Context, *connect.Request[v1.CreateClusterGatewayRequest]) (*connect.Response[v1.CreateClusterGatewayResponse], error) {
+	return nil, connect.NewError(connect.CodeUnimplemented, errors.New("chalk.server.v1.BuilderService.CreateClusterGateway is not implemented"))
 }
