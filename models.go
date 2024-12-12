36d7550f0d327aa8bde4c814b4947e7bb3edda5c
rooftoofwoof <jinhang@chalk.ai>
2024-12-12 11:03:03 -0800

    remove now-irrelevant comment

--- models.go
+++ models.go
@@ -359,12 +359,6 @@ type OnlineQueryBulkResult struct {
 //  2. UnmarshalInto also returns a ClientError if its argument is not a pointer to a list of
 //     structs.
 //
-//  3. UnmarshalInto does not currently handle unmarshalling:
-//     a. has-many features
-//     In the meantime, you can manually unmarshal a has-many feature across all root
-//     structs by using the UnmarshalTableInto function.
-//     b. struct-like features, such as Python `dataclass` or Pydantic models
-//
 // Usage example:
 //
 //	func printUserDetails(chalkClient chalk.Client) {
