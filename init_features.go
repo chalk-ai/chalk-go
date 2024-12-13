574a39f8584f4226d2ebbf10bd385a5f4c08b69e
rooftoofwoof <jinhang@chalk.ai>
2024-12-12 18:58:15 -0800

    make initFeatures a function not method

--- init_features.go
+++ init_features.go
@@ -12,7 +12,7 @@ import (
 
 func InitFeatures[T any](t *T) error {
 	structValue := reflect.ValueOf(t).Elem()
-	return newFeatureInitializer().initFeatures(structValue, "", make(map[string]bool), nil)
+	return initFeatures(structValue, "", make(map[string]bool))
 }
 
 type scopeTrie struct {
@@ -120,84 +120,14 @@ func (fi *featureInitializer) initRemoteFeatureMap(
 	return nil
 }
 
-//func (fi *featureInitializer) initFeaturesScoped(
-//	structValue reflect.Value,
-//	cumulativeFqn string,
-//	visited map[string]bool,
-//	scope *scopeTrie,
-//	nsMemo internal.NamespaceMemo,
-//) error {
-//	if structValue.Kind() != reflect.Struct {
-//		return fmt.Errorf(
-//			"feature initialization function argument must be a reflect.Value"+
-//				" of the kind reflect.Struct, found %s instead",
-//			structValue.Kind().String(),
-//		)
-//	}
-//
-//	structName := structValue.Type().Name()
-//	if isVisited, ok := visited[structName]; ok && isVisited {
-//		// Found a cycle. Just return.
-//		return nil
-//	}
-//	visited[structName] = true
-//	defer func() {
-//		visited[structName] = false
-//	}()
-//
-//	memo := nsMemo[structName]
-//	for resolvedFieldName, nextScope := range scope.children {
-//		updatedFqn := fmt.Sprintf("%s.%s", cumulativeFqn, resolvedFieldName)
-//
-//		fieldIdx, ok := memo.ResolvedFieldNameToIndices[resolvedFieldName]
-//		if !ok {
-//			return errors.Newf(
-//				"getting field index from memo, field '%s' not found among keys: %v",
-//				resolvedFieldName,
-//				colls.Keys(memo.ResolvedFieldNameToIndices),
-//			)
-//		}
-//		f := structValue.Field(fieldIdx)
-//		if !f.CanSet() {
-//			continue
-//		}
-//
-//		typ := f.Type()
-//		if f.Kind() != reflect.Map {
-//			if ptrErr := pointerCheck(f); ptrErr != nil {
-//				return ptrErr
-//			}
-//			typ = f.Type().Elem()
-//		}
-//
-//		if typ.Kind() == reflect.Struct &&
-//			typ != reflect.TypeOf(time.Time{}) &&
-//			!internal.IsTypeDataclass(typ) {
-//			if !f.IsNil() {
-//				return errors.Newf("struct with FQN '%s' should be nil", updatedFqn)
-//			}
-//			featureSet := reflect.New(f.Type().Elem())
-//			f.Set(featureSet)
-//			if err := fi.initFeaturesScoped(f.Elem(), updatedFqn, visited, nextScope, nsMemo); err != nil {
-//				return err
-//			}
-//		} else {
-//			fi.fieldsMap[updatedFqn] = append(fi.fieldsMap[updatedFqn], f)
-//		}
-//	}
-//	return nil
-//}
-
 // initFeatures is a recursive function that initializes all features
 // in the struct that is passed in. Each feature is initialized as a
 // pointer to a Feature struct with the appropriate FQN.
-func (fi *featureInitializer) initFeatures(
+func initFeatures(
 	structValue reflect.Value,
 	cumulativeFqn string,
 	visited map[string]bool,
-	scope *scopeTrie,
 ) error {
-	isScoped := scope != nil
 	if structValue.Kind() != reflect.Struct {
 		return fmt.Errorf(
 			"feature initialization function argument must be a reflect.Value"+
@@ -248,27 +178,7 @@ func (fi *featureInitializer) initFeatures(
 			continue
 		}
 
-		inScope := true
-		if scope != nil {
-			_, inScope = scope.children[resolvedName]
-		}
-
-		if f.Kind() == reflect.Ptr && internal.IsTypeDataclass(f.Type().Elem()) && isScoped {
-			// If dataclasses are being initialized for purposes
-			// of specifying query params, we want it to go to
-			// the next block where we initialize it the same way
-			// as a struct.
-			//
-			// If dataclass child fields are being selectively
-			// initialized for purposes of deserialization of
-			// query results, we simply return the field, as we
-			// do in this block, and let the feature setter set
-			// the value of the dataclass down the line.
-			if !inScope {
-				continue
-			}
-			fi.fieldsMap[updatedFqn] = append(fi.fieldsMap[updatedFqn], f)
-		} else if f.Type().Elem().Kind() == reflect.Struct &&
+		if f.Type().Elem().Kind() == reflect.Struct &&
 			f.Type().Elem() != reflect.TypeOf(time.Time{}) {
 			// RECURSIVE CASE.
 			// Create new Feature Set instance and point to it.
@@ -276,34 +186,17 @@ func (fi *featureInitializer) initFeatures(
 			//
 			//      Features.User.CreditReport = new(CreditReport)
 			//
-			if !inScope {
-				continue
-			}
 			if ptrErr := pointerCheck(f); ptrErr != nil {
 				return ptrErr
 			}
 
-			if isScoped {
-				if f.IsNil() {
-					featureSet := reflect.New(f.Type().Elem())
-					f.Set(featureSet)
-				}
-				newScope := scope.children[resolvedName]
-				if newScope == nil {
-					return errors.Newf("scope not found for feature '%s'", cumulativeFqn)
-				}
-				if err := fi.initFeatures(f.Elem(), updatedFqn, visited, newScope); err != nil {
-					return err
-				}
-			} else {
-				featureSet := reflect.New(f.Type().Elem())
-				// TODO: This is an actual feature set we don't have to disguise it, just have to set it.
-				ptrInDisguiseToFeatureSet := reflect.NewAt(f.Type().Elem(), featureSet.UnsafePointer())
-				f.Set(ptrInDisguiseToFeatureSet)
-				featureSetInDisguise := f.Elem()
-				if err := fi.initFeatures(featureSetInDisguise, updatedFqn, visited, nil); err != nil {
-					return err
-				}
+			featureSet := reflect.New(f.Type().Elem())
+			// TODO: This is an actual feature set we don't have to disguise it, just have to set it.
+			ptrInDisguiseToFeatureSet := reflect.NewAt(f.Type().Elem(), featureSet.UnsafePointer())
+			f.Set(ptrInDisguiseToFeatureSet)
+			featureSetInDisguise := f.Elem()
+			if err := initFeatures(featureSetInDisguise, updatedFqn, visited); err != nil {
+				return err
 			}
 		} else if f.Kind() == reflect.Map {
 			// Creates a map of tag values to pointers to Features.
@@ -337,56 +230,36 @@ func (fi *featureInitializer) initFeatures(
 				}
 				updatedResolvedName := fmt.Sprintf("%s__%d__", resolvedName, seconds)
 				bucketFqn := fmt.Sprintf("%s.%s", cumulativeFqn, updatedResolvedName)
-				if isScoped {
-					if _, bucketInScope := scope.children[updatedResolvedName]; !bucketInScope {
-						continue
-					}
-					// Make map only if one of the bucket features need to be set
-					// If no bucket features need to be set, map is nil.
-					if f.IsNil() {
-						f.Set(reflect.MakeMap(f.Type()))
-					}
-					fi.fieldsMap[bucketFqn] = append(fi.fieldsMap[bucketFqn], f)
-				} else {
-					if f.IsNil() {
-						f.Set(reflect.MakeMap(f.Type()))
-					}
-					feature := Feature{Fqn: bucketFqn}
-					ptrInDisguiseToFeature := reflect.NewAt(mapValueType.Elem(), reflect.ValueOf(&feature).UnsafePointer())
-					f.SetMapIndex(reflect.ValueOf(tag), ptrInDisguiseToFeature)
+				if f.IsNil() {
+					f.Set(reflect.MakeMap(f.Type()))
 				}
-
+				feature := Feature{Fqn: bucketFqn}
+				ptrInDisguiseToFeature := reflect.NewAt(mapValueType.Elem(), reflect.ValueOf(&feature).UnsafePointer())
+				f.SetMapIndex(reflect.ValueOf(tag), ptrInDisguiseToFeature)
 			}
 		} else {
 			// BASE CASE.
-			if !inScope {
-				continue
-			}
 			if ptrErr := pointerCheck(f); ptrErr != nil {
 				return ptrErr
 			}
 
-			if isScoped {
-				fi.fieldsMap[updatedFqn] = append(fi.fieldsMap[updatedFqn], f)
-			} else {
-				// Create new Feature instance and point to it.
-				// The equivalent way of doing it without 'reflect':
-				//
-				//      Features.User.CreditReport.Id = (*string)(unsafe.Pointer(&Feature{"user.credit_report.id"}))
-				//
+			// Create new Feature instance and point to it.
+			// The equivalent way of doing it without 'reflect':
+			//
+			//      Features.User.CreditReport.Id = (*string)(unsafe.Pointer(&Feature{"user.credit_report.id"}))
+			//
 
-				// Dataclass fields are not actually real features,
-				// so when we are initializing the root Features struct,
-				// we want to return the parent (real) feature FQN
-				// instead of the fake FQN of the dataclass child field.
-				if internal.IsDataclass(structValue) {
-					updatedFqn = DesuffixFqn(updatedFqn)
-				}
-
-				feature := Feature{Fqn: updatedFqn}
-				ptrInDisguiseToFeature := reflect.NewAt(f.Type().Elem(), reflect.ValueOf(&feature).UnsafePointer())
-				f.Set(ptrInDisguiseToFeature)
+			// Dataclass fields are not actually real features,
+			// so when we are initializing the root Features struct,
+			// we want to return the parent (real) feature FQN
+			// instead of the fake FQN of the dataclass child field.
+			if internal.IsDataclass(structValue) {
+				updatedFqn = DesuffixFqn(updatedFqn)
 			}
+
+			feature := Feature{Fqn: updatedFqn}
+			ptrInDisguiseToFeature := reflect.NewAt(f.Type().Elem(), reflect.ValueOf(&feature).UnsafePointer())
+			f.Set(ptrInDisguiseToFeature)
 		}
 	}
 	return nil
