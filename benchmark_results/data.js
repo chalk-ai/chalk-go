b41a04020bbeeb8b69078c680081c1bc4682d58a
github-action-benchmark <github@users.noreply.github.com>
2025-02-12 02:01:16 +0000

    add Benchmark (go) benchmark result for cab0fe7b237daf679701044cdc8e1de669986986

---
+++ benchmark_results/data.js
@@ -0,0 +1,40 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1739325676860,
+  "repoUrl": "https://github.com/chalk-ai/chalk-go",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "chalk-ai",
+            "username": "chalk-ai"
+          },
+          "committer": {
+            "name": "chalk-ai",
+            "username": "chalk-ai"
+          },
+          "id": "cab0fe7b237daf679701044cdc8e1de669986986",
+          "message": "jh/bench",
+          "timestamp": "2025-02-12T01:57:50Z",
+          "url": "https://github.com/chalk-ai/chalk-go/pull/285/commits/cab0fe7b237daf679701044cdc8e1de669986986"
+        },
+        "date": 1739325675872,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkUnmarshalMultiNsPrimitives",
+            "value": 64771342,
+            "unit": "ns/op",
+            "extra": "16 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkUnmarshalMultiNsWindowed",
+            "value": 83843219,
+            "unit": "ns/op",
+            "extra": "13 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
