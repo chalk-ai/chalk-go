b4a5c3120ce2c216a023b5d7c9691515d033fa33
rooftoofwoof <jinhang@chalk.ai>
2025-02-13 18:16:05 -0800

    default to true

--- serializers.go
+++ serializers.go
@@ -51,7 +51,9 @@ func (p OnlineQueryParams) serialize() (*internal.OnlineQueryRequestSerialized, 
 	var encodingOptions internal.FeatureEncodingOptions
 	if p.EncodingOptions == nil {
 		encodingOptions = internal.FeatureEncodingOptions{
-			EncodeStructsAsObjects: false,
+			// Default to true to ensure backcompat.
+			// See https://github.com/chalk-ai/chalk-go/pull/159
+			EncodeStructsAsObjects: true,
 		}
 	} else {
 		encodingOptions = internal.FeatureEncodingOptions{
