8114510302b4956d7da231780b8eaa168b49e082
rooftoofwoof <jinhang@chalk.ai>
2025-03-05 17:39:26 -0800

    test diff

--- internal/memo_namespace.go
+++ internal/memo_namespace.go
@@ -30,61 +30,6 @@ func (m *NamespaceMemosT) LoadOrStoreWithVisited(structType reflect.Type, visite
 	})
 }
 
-func populateNamespaceMemoStruct(typ reflect.Type, visited map[reflect.Type]bool) (*NamespaceMemo, error) {
-	if visited == nil {
-		visited = map[reflect.Type]bool{}
-	}
-	structName := typ.Name()
-	namespace := ChalkpySnakeCase(structName)
-	nsMemo := NamespaceMemo{
-		ResolvedFieldNameToIndices: map[string][]int{},
-		StructFieldsSet:            map[string]bool{},
-	}
-	for fieldIdx := 0; fieldIdx < typ.NumField(); fieldIdx++ {
-		fm := typ.Field(fieldIdx)
-		resolvedName, err := ResolveFeatureName(fm)
-		if err != nil {
-			return nil, errors.Wrapf(err, "error resolving feature name: %s", fm.Name)
-		}
-		nsMemo.ResolvedFieldNameToIndices[resolvedName] = append(nsMemo.ResolvedFieldNameToIndices[resolvedName], fieldIdx)
-		// Has-many features come back as a list of structs whose keys are namespaced FQNs.
-		// Here we map those keys to their respective indices in the struct, so that we
-		// don't have to do any string manipulation to deprefix the FQN when unmarshalling.
-		rootFqn := namespace + "." + resolvedName
-		nsMemo.ResolvedFieldNameToIndices[rootFqn] = append(nsMemo.ResolvedFieldNameToIndices[rootFqn], fieldIdx)
-
-		// Handle exploding windowed features
-		if fm.Type.Kind() == reflect.Map {
-			// Is a windowed feature
-			intTags, err := GetWindowBucketsSecondsFromStructTag(fm)
-			if err != nil {
-				return nil, errors.Wrapf(
-					err,
-					"error getting window buckets for field '%s' in struct '%s'",
-					fm.Name,
-					structName,
-				)
-			}
-			for _, tag := range intTags {
-				bucketFqn := resolvedName + "__" + strconv.Itoa(tag) + "__"
-				nsMemo.ResolvedFieldNameToIndices[bucketFqn] = append(nsMemo.ResolvedFieldNameToIndices[bucketFqn], fieldIdx)
-				rootBucketFqn := namespace + "." + bucketFqn
-				nsMemo.ResolvedFieldNameToIndices[rootBucketFqn] = append(nsMemo.ResolvedFieldNameToIndices[rootBucketFqn], fieldIdx)
-			}
-		} else {
-			if err := PopulateNamespaceMemos(fm.Type, visited); err != nil {
-				return nil, errors.Wrapf(err, "populating namespace memo for field '%s'", fm.Name)
-			}
-		}
-
-		if fm.Type.Kind() == reflect.Ptr && IsStruct(fm.Type.Elem()) && !IsTypeDataclass(fm.Type.Elem()) {
-			nsMemo.StructFieldsSet[resolvedName] = true
-		}
-	}
-
-	return &nsMemo, nil
-}
-
 /*PopulateNamespaceMemos populates a memo for each struct to make bulk-unmarshalling and has-many unmarshalling
 * efficient. i.e. Given:
  *  type User struct {
@@ -141,4 +86,59 @@ func PopulateNamespaceMemos(typ reflect.Type, visited map[reflect.Type]bool) err
 		}
 	}
 	return nil
+}
+
+func populateNamespaceMemoStruct(typ reflect.Type, visited map[reflect.Type]bool) (*NamespaceMemo, error) {
+	if visited == nil {
+		visited = map[reflect.Type]bool{}
+	}
+	structName := typ.Name()
+	namespace := ChalkpySnakeCase(structName)
+	nsMemo := NamespaceMemo{
+		ResolvedFieldNameToIndices: map[string][]int{},
+		StructFieldsSet:            map[string]bool{},
+	}
+	for fieldIdx := 0; fieldIdx < typ.NumField(); fieldIdx++ {
+		fm := typ.Field(fieldIdx)
+		resolvedName, err := ResolveFeatureName(fm)
+		if err != nil {
+			return nil, errors.Wrapf(err, "error resolving feature name: %s", fm.Name)
+		}
+		nsMemo.ResolvedFieldNameToIndices[resolvedName] = append(nsMemo.ResolvedFieldNameToIndices[resolvedName], fieldIdx)
+		// Has-many features come back as a list of structs whose keys are namespaced FQNs.
+		// Here we map those keys to their respective indices in the struct, so that we
+		// don't have to do any string manipulation to deprefix the FQN when unmarshalling.
+		rootFqn := namespace + "." + resolvedName
+		nsMemo.ResolvedFieldNameToIndices[rootFqn] = append(nsMemo.ResolvedFieldNameToIndices[rootFqn], fieldIdx)
+
+		// Handle exploding windowed features
+		if fm.Type.Kind() == reflect.Map {
+			// Is a windowed feature
+			intTags, err := GetWindowBucketsSecondsFromStructTag(fm)
+			if err != nil {
+				return nil, errors.Wrapf(
+					err,
+					"error getting window buckets for field '%s' in struct '%s'",
+					fm.Name,
+					structName,
+				)
+			}
+			for _, tag := range intTags {
+				bucketFqn := resolvedName + "__" + strconv.Itoa(tag) + "__"
+				nsMemo.ResolvedFieldNameToIndices[bucketFqn] = append(nsMemo.ResolvedFieldNameToIndices[bucketFqn], fieldIdx)
+				rootBucketFqn := namespace + "." + bucketFqn
+				nsMemo.ResolvedFieldNameToIndices[rootBucketFqn] = append(nsMemo.ResolvedFieldNameToIndices[rootBucketFqn], fieldIdx)
+			}
+		} else {
+			if err := PopulateNamespaceMemos(fm.Type, visited); err != nil {
+				return nil, errors.Wrapf(err, "populating namespace memo for field '%s'", fm.Name)
+			}
+		}
+
+		if fm.Type.Kind() == reflect.Ptr && IsStruct(fm.Type.Elem()) && !IsTypeDataclass(fm.Type.Elem()) {
+			nsMemo.StructFieldsSet[resolvedName] = true
+		}
+	}
+
+	return &nsMemo, nil
 }
