a2cc295cf058b6ede9e174d666ad13dafb86cb8a
rooftoofwoof <jinhang@chalk.ai>
2025-03-28 19:38:18 -0700

    fix erring hm test

--- internal/tests/integration/resolver_exception_test.go
+++ internal/tests/integration/resolver_exception_test.go
@@ -43,22 +43,39 @@ func TestErringScalar(t *testing.T) {
 	}
 }
 
-// TestOnlineQueryGrpcErringHasMany tests requests with an erring has-many feature as the sole output
-func TestOnlineQueryGrpcErringHasMany(t *testing.T) {
+// TestErringHasMany tests requests with an erring has-many feature as the sole output
+func TestErringHasMany(t *testing.T) {
+	t.Parallel()
 	SkipIfNotIntegrationTester(t)
-	params := chalk.OnlineQueryParams{}.
-		WithInput(testFeatures.CrashingHasManyRoot.Id, []string{"id_a"}).
-		WithOutputs(testFeatures.CrashingHasManyRoot.CrashingHasMany)
-	resp, err := grpcClient.OnlineQueryBulk(context.Background(), params)
-	assert.Error(t, err)
 
-	row, err := resp.GetRow(0)
-	assert.NoError(t, err)
+	for _, useGrpc := range []bool{true, false} {
+		t.Run(fmt.Sprintf("grpc=%v", useGrpc), func(t *testing.T) {
+			t.Parallel()
+			if useGrpc {
+				bulkParams := chalk.OnlineQueryParams{}.
+					WithInput(testFeatures.CrashingHasManyRoot.Id, []string{"id_a"}).
+					WithOutputs(testFeatures.CrashingHasManyRoot.CrashingHasMany)
+				resp, err := grpcClient.OnlineQueryBulk(context.Background(), bulkParams)
+				assert.Error(t, err)
+				assert.Contains(t, err.Error(), "42")
 
-	hmFeat, err := row.GetFeature(testFeatures.CrashingHasManyRoot.CrashingHasMany)
-	assert.NoError(t, err)
-	assert.NotNil(t, hmFeat)
-	castVal, ok := hmFeat.Value.([]any)
-	assert.True(t, ok)
-	assert.Equal(t, 0, len(castVal))
+				row, err := resp.GetRow(0)
+				assert.NoError(t, err)
+
+				hmFeat, err := row.GetFeature(testFeatures.CrashingHasManyRoot.CrashingHasMany)
+				assert.NoError(t, err)
+				assert.NotNil(t, hmFeat)
+				castVal, ok := hmFeat.Value.([]any)
+				assert.True(t, ok)
+				assert.Equal(t, 0, len(castVal))
+			} else {
+				singularParams := chalk.OnlineQueryParams{}.
+					WithInput(testFeatures.CrashingHasManyRoot.Id, "id_a").
+					WithOutputs(testFeatures.CrashingHasManyRoot.CrashingHasMany)
+				_, err := restClient.OnlineQuery(context.Background(), singularParams, nil)
+				assert.Error(t, err)
+				assert.Contains(t, err.Error(), "42")
+			}
+		})
+	}
 }
