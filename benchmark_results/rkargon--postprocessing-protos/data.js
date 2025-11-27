4ded819fad6a693d51a980036e224696e6f5c0b3
github-action-benchmark <github@users.noreply.github.com>
2025-11-27 01:56:34 +0000

    add Benchmark (go) benchmark result for 35ccd3b3d30a1218bd219f20fe643d09efa7c110

--- benchmark_results/rkargon--postprocessing-protos/data.js
+++ benchmark_results/rkargon--postprocessing-protos/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764208488720,
+  "lastUpdate": 1764208593935,
   "repoUrl": "https://github.com/chalk-ai/chalk-go",
   "entries": {
     "Benchmark": [
@@ -28644,6 +28644,56 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkMakeRecordManyRowsAllTypes",
             "value": 348.1,
+            "unit": "ms/op",
+            "extra": "6 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "chalk-ai",
+            "username": "chalk-ai"
+          },
+          "committer": {
+            "name": "chalk-ai",
+            "username": "chalk-ai"
+          },
+          "id": "35ccd3b3d30a1218bd219f20fe643d09efa7c110",
+          "message": "Generate protos for resolver postprocessing",
+          "timestamp": "2025-11-27T00:59:12Z",
+          "url": "https://github.com/chalk-ai/chalk-go/pull/528/commits/35ccd3b3d30a1218bd219f20fe643d09efa7c110"
+        },
+        "date": 1764208593850,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkConvertBytesToTable",
+            "value": 0.9878,
+            "unit": "ms/op",
+            "extra": "1086 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkConvertBytesToTableParallel",
+            "value": 112.2,
+            "unit": "ms/op",
+            "extra": "10 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkMakeRecordSingleRowPrimitives",
+            "value": 36.73,
+            "unit": "ms/op",
+            "extra": "36 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkMakeRecordSingleRowAllTypes",
+            "value": 211.1,
+            "unit": "ms/op",
+            "extra": "7 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkMakeRecordManyRowsAllTypes",
+            "value": 353.1,
             "unit": "ms/op",
             "extra": "6 times\n4 procs"
           }
