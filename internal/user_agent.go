cf0beee7583d08053d7fa586e84a21a6d4532c07
Elliot Marx <elliot@chalk.ai>
2026-02-04 13:04:55 -0800

    wip

--- internal/user_agent.go
+++ internal/user_agent.go
@@ -6,7 +6,6 @@ import (
 )
 
 const (
-	chalkModulePath     = "github.com/chalk-ai/chalk-go"
 	defaultChalkVersion = "dev"
 	userAgentPrefix     = "chalk-go/"
 )
@@ -24,42 +23,13 @@ func UserAgent() string {
 }
 
 func chalkVersion() string {
-	if v := versionFromBuildInfo(); v != "" {
-		return v
-	}
-	return defaultChalkVersion
-}
-
-func versionFromBuildInfo() string {
 	info, ok := debug.ReadBuildInfo()
 	if !ok {
-		return ""
+		return defaultChalkVersion
 	}
-
-	if v := moduleVersion(info.Main); v != "" {
-		return v
-	}
-	for _, dep := range info.Deps {
-		if dep == nil {
-			continue
-		}
-		if v := moduleVersion(*dep); v != "" {
-			return v
-		}
-	}
-	return ""
-}
-
-func moduleVersion(mod debug.Module) string {
-	if mod.Path != chalkModulePath {
-		return ""
-	}
-	version := mod.Version
-	if version == "" && mod.Replace != nil {
-		version = mod.Replace.Version
-	}
+	version := info.Main.Version
 	if version == "" || version == "(devel)" {
-		return ""
+		return defaultChalkVersion
 	}
 	return version
 }
