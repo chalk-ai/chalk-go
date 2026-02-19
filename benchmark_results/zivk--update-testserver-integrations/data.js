df7cfbdde28d8db06481b1abd7f1fa9f82bf10b5
github-action-benchmark <github@users.noreply.github.com>
2026-02-19 03:21:16 +0000

    add Benchmark (go) benchmark result for 43e38b34fbe83d835ad7b169c0ae75931bd26f7b

--- benchmark_results/zivk--update-testserver-integrations/data.js
+++ benchmark_results/zivk--update-testserver-integrations/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1771447060974,
+  "lastUpdate": 1771471276249,
   "repoUrl": "https://github.com/chalk-ai/chalk-go",
   "entries": {
     "Benchmark": [
@@ -31944,6 +31944,56 @@ window.BENCHMARK_DATA = {
             "value": 295,
             "unit": "ms/op",
             "extra": "4 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "chalk-ai",
+            "username": "chalk-ai"
+          },
+          "committer": {
+            "name": "chalk-ai",
+            "username": "chalk-ai"
+          },
+          "id": "43e38b34fbe83d835ad7b169c0ae75931bd26f7b",
+          "message": "update testserver with additional integration rpc",
+          "timestamp": "2026-02-18T20:37:04Z",
+          "url": "https://github.com/chalk-ai/chalk-go/pull/587/commits/43e38b34fbe83d835ad7b169c0ae75931bd26f7b"
+        },
+        "date": 1771471276147,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkConvertBytesToTable",
+            "value": 1.278,
+            "unit": "ms/op",
+            "extra": "942 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkConvertBytesToTableParallel",
+            "value": 125.5,
+            "unit": "ms/op",
+            "extra": "10 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkMakeRecordSingleRowPrimitives",
+            "value": 33.18,
+            "unit": "ms/op",
+            "extra": "39 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkMakeRecordSingleRowAllTypes",
+            "value": 209.8,
+            "unit": "ms/op",
+            "extra": "7 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkMakeRecordManyRowsAllTypes",
+            "value": 315.7,
+            "unit": "ms/op",
+            "extra": "6 times\n4 procs"
           }
         ]
       }
